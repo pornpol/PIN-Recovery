f8f4de52335d825a6dfed62c79ed3e53eca6a33a
Pornpol Wasuwat <pornpol.w@gmail.com>
2021-12-14 03:47:14 +0000

    change key from asymmetric key to random number

--- index.js
+++ index.js
@@ -5,22 +5,8 @@ const atob = require('atob');
 
 const OFFSET = 128;
 
-// Generate Private Key
-const secretEd25519 = crypto.generateKeyPairSync('ed25519', {
-  privateKeyEncoding: {
-    type: 'pkcs8',
-    format: 'der',
-  }
-}).privateKey.toString('base64');
-const secretRsa = crypto.generateKeyPairSync('rsa', {
-  modulusLength: 4096,
-  privateKeyEncoding: {
-    type: 'pkcs8',
-    format: 'der',
-  }
-}).privateKey.toString('base64');
-console.log(`secretEd25519: ${secretEd25519}`);
-// console.log(`secretRsa: ${secretRsa}`);
+const key = crypto.randomBytes(32).toString('base64');
+console.log(`key: ${key}\n`);
 
 const cid1 = '1111111111111';
 const cid2 = '2222222222222';
@@ -28,30 +14,25 @@ const cid2 = '2222222222222';
 const salt = crypto.randomBytes(32).toString('base64');
 console.log(`salt: ${salt}\n`);
 
-// Generate base58 PIN from Private Key & Salt
-const pinEd25519 = bs58.encode(crypto
-                                .createHash('sha256')
-                                .update(secretEd25519.concat(salt))
-                                .digest())
-                    .substring(0, 6);
-const pinRsa = bs58.encode(crypto
-                                .createHash('sha256')
-                                .update(secretRsa.concat(salt))
-                                .digest())
-                    .substring(0, 6);
-console.log(`pinEd25519: ${pinEd25519}`)
-// console.log(`pinRsa: ${pinRsa}\n`)
+// Generate base58 PIN from Key & Salt
+
+const pin = bs58.encode(crypto
+                          .createHash('sha256')
+                          .update(key.concat(salt))
+                          .digest())
+              .substring(0, 6);
+console.log(`pin: ${pin}\n`)
 
 // Create Sub Share
 // Modified Polynomial Equation: 2 shares (cid, share) -> Linear
-// x = 0: Private Key
+// x = 0: Key
 // x = 1: Citizen ID
 // x = 2: Share
 const createSubShares = (subSecret, subCid) =>
                           [1,2].map((x) =>
                             ((x) * (subCid - subSecret)) + subSecret);
 
-// Create Share from Private Key & CID
+// Create Share from Key & CID
 // Calculate Share digit by digit
 const createShares = (secret, cid) => {
   const subSecrets = Buffer.from(secret, 'base64').toString('hex').split('');
@@ -63,7 +44,7 @@ const createShares = (secret, cid) => {
   return btoa(subShares.join(''));
 }
 
-// Generate Private Key from CID & share
+// Generate Key from CID & share
 const recoverSecret = (cid, share) => {
   const subShares = atob(share).split('')
   const key = subShares.map((s, index) =>
@@ -74,31 +55,16 @@ const recoverSecret = (cid, share) => {
 }
 
 ///////////////////////// Test ED25519 //////////////////////////////
-const share1 = createShares(secretEd25519, cid1);
-const share2 = createShares(secretEd25519, cid2);
+const share1 = createShares(key, cid1);
+const share2 = createShares(key, cid2);
 console.log(`share1: ${share1}`);
-// console.log(`share2: ${share2}\n`);
+console.log(`share2: ${share2}\n`);
 
 const recoverSecret1 = recoverSecret(cid1, share1);
 const recoverSecret2 = recoverSecret(cid2, share2);
-// console.log(`recoverSecret1: ${recoverSecret1}`);
-// console.log(`recoverSecret2: ${recoverSecret2}\n`);
+console.log(`recoverSecret1: ${recoverSecret1}`);
+console.log(`recoverSecret2: ${recoverSecret2}\n`);
 
 console.log('recoverSecret ED25519 match: ',
-  (secretEd25519 === recoverSecret1) && (secretEd25519 === recoverSecret2));
+  (key === recoverSecret1) && (key === recoverSecret2));
 //////////////////////////////////////////////////////////////////////
-
-///////////////////////// Test RSA 4096 //////////////////////////////
-const share3 = createShares(secretRsa, cid1);
-const share4 = createShares(secretRsa, cid2);
-// console.log(`share3: ${share3}`);
-// console.log(`share4: ${share4}\n`);
-
-const recoverSecret3 = recoverSecret(cid1, share3);
-const recoverSecret4 = recoverSecret(cid2, share4);
-// console.log(`recoverSecret3: ${recoverSecret3}`);
-// console.log(`recoverSecret4: ${recoverSecret4}\n`);
-
-// console.log('recoverSecret RSA match: ',
-//   (secretRsa === recoverSecret3) && (secretRsa === recoverSecret4));
-////////////////////////////////////////////////////////////////////
